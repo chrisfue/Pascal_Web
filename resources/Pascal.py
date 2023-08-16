1873d389e0fb58fe6444fe7b9a415725331306eb
ChristophTheFirst <ic21b028@technikum-wien-at>
2023-08-16 15:43:21 +0200

    tflite

--- resources/Pascal.py
+++ resources/Pascal.py
@@ -3,60 +3,59 @@ import json
 import pickle
 import numpy as np
 
+import tensorflow as tf
 import nltk
 from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
 
 
 
-
-
-
-
 class Chatbot:
 
-    def __init__(self,resource_path):
-        
-        self.lemmatizer = WordNetLemmatizer()
-        self.intents = json.loads(open(str(resource_path)+'intents.json').read())
-    
-        self.words = pickle.load(open(str(resource_path)+'words.pkl','rb'))
-        self.classes = pickle.load(open(str(resource_path)+'classes.pkl','rb'))
+    def __init__(self, resource_path):
+        self.lemmatizer = tf.lite.Interpreter(model_path=str(resource_path) + 'model.tflite')
+        self.intents = json.loads(open(str(resource_path) + 'intents.json').read())
+        self.words = pickle.load(open(str(resource_path) + 'words.pkl', 'rb'))
+        self.classes = pickle.load(open(str(resource_path) + 'classes.pkl', 'rb'))
+        self.lemmatizer_nltk = WordNetLemmatizer()
 
-        self.model =load_model(str(resource_path)+'model.h5')
-
-    def clean_up_sentence(self,sentence):
-        sentence_words =nltk.word_tokenize(sentence)
-        sentence_words = [ self.lemmatizer.lemmatize(word) for word in sentence_words]
+    def clean_up_sentence(self, sentence):
+        sentence_words = nltk.word_tokenize(sentence)
+        sentence_words = [self.lemmatizer_nltk.lemmatize(word) for word in sentence_words]
         return sentence_words
 
-    def bag_of_Words(self,sentence):
+    def bag_of_Words(self, sentence):
         sentence_words = self.clean_up_sentence(sentence)
-        bag = [0]*len(self.words)
+        bag = [0] * len(self.words)
         for w in sentence_words:
-            for i,word in enumerate(self.words):
-                if word ==w:
-                    bag[i]=1
+            for i, word in enumerate(self.words):
+                if word == w:
+                    bag[i] = 1
         return np.array(bag)
 
-    def predict_class(self,sentence):
+    def predict_class(self, sentence):
         bow = self.bag_of_Words(sentence)
-        res = self.model.predict(np.array([bow]),verbose=0)[0]
-        ERROR_THRESHOLD = 0.3
-        results = [[i,r] for i,r in enumerate(res)if r > ERROR_THRESHOLD]
+        self.lemmatizer.allocate_tensors()
+        input_details = self.lemmatizer.get_input_details()
+        output_details = self.lemmatizer.get_output_details()
 
-        results.sort(key=lambda x: x[1],reverse=True)
-        return_list =[]
+        self.lemmatizer.set_tensor(input_details[0]['index'], np.array([bow], dtype=np.float32))
+        self.lemmatizer.invoke()
+        res = self.lemmatizer.get_tensor(output_details[0]['index'])[0]
+
+        ERROR_THRESHOLD = 0.3
+        results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+        results.sort(key=lambda x: x[1], reverse=True)
+        return_list = []
         for r in results:
-            return_list.append({'intent':self.classes[r[0]],'propability':str(r[1])})
+            return_list.append({'intent': self.classes[r[0]], 'probability': str(r[1])})
         return return_list
 
-    def get_response(self,intents_list, intents_json):
-        tag= intents_list[0]['intent']
+    def get_response(self, intents_list, intents_json):
+        tag = intents_list[0]['intent']
         list_of_intents = intents_json['intents']
         for i in list_of_intents:
             if i['tag'] == tag:
                 result = random.choice(i['responses'])
                 break
         return result
-
