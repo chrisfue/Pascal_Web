ffca5c07eaa8bdd72827524d80af5ab05448ccf0
ChristophTheFirst <ic21b028@technikum-wien-at>
2023-08-17 12:57:11 +0200

    clear list

--- app.py
+++ app.py
@@ -27,6 +27,9 @@ def index():
         
         bot_response = pascal.get_response(ints,pascal.intents)
         chat_history.append(("bot", bot_response))
+    
+    else: 
+        chat_history.clear
 
     return render_template('index.html', chat_history=chat_history)
 
