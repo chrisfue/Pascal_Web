d63e8d4505b350fae62f789db21c1d12700f07fe
ChristophTheFirst <ic21b028@technikum-wien-at>
2023-08-17 12:44:43 +0200

    no clear

--- app.py
+++ app.py
@@ -22,15 +22,13 @@ def index():
         user_message = request.form['user_input']
         chat_history.append(("user", user_message))
 
-        
+        # Simulate bot response for demonstration purposes
         ints = pascal.predict_class(user_message)
         
         
         bot_response = pascal.get_response(ints,pascal.intents)
         chat_history.append(("bot", bot_response))
-    else:
-        chat_history=[]
-
+    
     return render_template('index.html', chat_history=chat_history)
 
 
